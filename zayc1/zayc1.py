39e743392c829dbe7132b7ec98f38fe33eaceb82
LkkB <lkkboris@gmail.com>
2026-03-11 12:06:29 +0300

    fvbv

--- zayc1/zayc1.py
+++ zayc1/zayc1.py
@@ -6,43 +6,27 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-def draw_body(surface, x, y, width, height, color):
+x, y = 200, 200
+width, height = 200, 400
+color = (200, 200, 200)
 
-    ellipse(surface, color, (x - width // 2, y - height // 2, width, height))
+body_width = width // 2
+body_height = height // 2
+body_y = y + body_height // 2
+ellipse(screen, color, (x - body_width // 2, body_y - body_height // 2, body_width, body_height))
 
-def draw_head(surface, x, y, size, color):
+head_size = height // 4
+circle(screen, color, (x, y - head_size // 2), head_size // 2)
 
-    circle(surface, color, (x, y), size // 2)
+ear_height = height // 3
+ear_y = y - height // 2 + ear_height // 2
+for ear_x in (x - head_size // 4, x + head_size // 4):
+    ellipse(screen, color, (ear_x - (width // 8) // 2, ear_y - ear_height // 2, width // 8, ear_height))
 
-def draw_ear(surface, x, y, width, height, color):
-
-    ellipse(surface, color, (x - width // 2, y - height // 2, width, height))
-
-def draw_leg(surface, x, y, width, height, color):
-
-    ellipse(surface, color, (x - width // 2, y - height // 2, width, height))
-
-def draw_hare(surface, x, y, width, height, color):
-
-    body_width = width // 2
-    body_height = height // 2
-    body_y = y + body_height // 2
-    draw_body(surface, x, body_y, body_width, body_height, color)
-
-    head_size = height // 4
-    draw_head(surface, x, y - head_size // 2, head_size, color)
-
-    ear_height = height // 3
-    ear_y = y - height // 2 + ear_height // 2
-    for ear_x in (x - head_size // 4, x + head_size // 4):
-        draw_ear(surface, ear_x, ear_y, width // 8, ear_height, color)
-
-    leg_height = height // 16
-    leg_y = y + height // 2 - leg_height // 2
-    for leg_x in (x - width // 4, x + width // 4):
-        draw_leg(surface, leg_x, leg_y, width // 4, leg_height, color)
-
-draw_hare(screen, 200, 200, 200, 400, (200, 200, 200))
+leg_height = height // 16
+leg_y = y + height // 2 - leg_height // 2
+for leg_x in (x - width // 4, x + width // 4):
+    ellipse(screen, color, (leg_x - (width // 4) // 2, leg_y - leg_height // 2, width // 4, leg_height))
 
 pygame.display.update()
 clock = pygame.time.Clock()
